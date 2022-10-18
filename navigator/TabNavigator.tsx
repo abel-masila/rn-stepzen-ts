ad2a5abec6438229237aa727fe2f0007872b53b2
Abel Masila <abel@x2ai.com>
2022-10-18 22:45:48 +0300

    added tab icons

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "@rneui/themed";
 import Customers from "../screens/Customers";
 import Orders from "../screens/Orders";
 
@@ -13,13 +14,40 @@ const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
   const navigation = useNavigation();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59c1cc",
+        tabBarInactiveTintColor: "gray",
+
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "#59c1cc" : "gray"}
+              />
+            );
+          } else if (route.name === "Orders") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "#eb6a7c" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Customers" component={Customers} />
       <Tab.Screen name="Orders" component={Orders} />
     </Tab.Navigator>
